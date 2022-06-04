58ad173c412dc3bbe8a87aa4c99eae04fcc3bfcc
AntonyBlanco <ablancot@unsa.edu.pe>
2022-06-04 01:58:36 -0500

    Implementacion de Node ara Stack y preparacion de clase Stack

---
+++ 2Pila/Node.java
@@ -0,0 +1,26 @@
+public class Node<E>{
+	private E data;
+	private Node<E> next;
+	
+	public Node(E data, Node<E> next) {
+		this.data = data;
+		this.next = next;
+	}
+	public E getData() {
+		return this.data;
+	}
+	public void setData(E data) {
+		this.data = data;
+	}
+	public Node<E> getNext() {
+		return this.next;
+	}
+	public void setNext(Node<E> next) {
+		this.next = next;
+	}
+
+	public String toString() {
+		return this.data.toString();
+	}
+}
+
---
+++ 2Pila/Stack.java
@@ -0,0 +1,28 @@
+public class Stack<E> {
+	private Node<E> root;
+
+	public Stack() {
+		root = null;
+	}
+	public boolean empty() {
+	// Retorna verdadero si la pila esta vacia
+		return false;
+	}
+	public E peek() {
+	// Retorna el objeto en el tope de la pila
+		return null;
+	}
+	public E pop() {
+	// Elimina el objeto en el tope de la pila y lo retorna
+		return null;
+	}
+	public E push() {
+	// Coloca un objeto en el tope de la pila
+		return null;
+	}
+	public int search() {
+	// Retorna la posicion basada en 1 del objeto en la pila
+		return 0;
+	}
+}
+
