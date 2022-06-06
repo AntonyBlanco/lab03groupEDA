93ab1b673c57b457e9ea31238dbe72cbb088a796
AntonyBlanco <ablancot@unsa.edu.pe>
2022-06-05 23:49:50 -0500

    Se modifico el Main de pila para demostrar e funcionamiento

--- Ejercicio2/2Pila/Main.java
+++ Ejercicio2/2Pila/Main.java
@@ -4,23 +4,33 @@ public class Main{
 		
 		System.out.println("Pila: " + pilaStr);
 		System.out.println("Tope: " + pilaStr.top());
+		System.out.println("esta vacio?: " + pilaStr.isEmpty());
 		
 		pilaStr.push("a");
 		
 		System.out.println("Pila: " + pilaStr);
 		System.out.println("Tope: " + pilaStr.top());
+		System.out.println("esta vacio?: " + pilaStr.isEmpty());
 		
 		pilaStr.push("b");
 		pilaStr.push("da");
-		pilaStr.push("testTop");
+		pilaStr.push("test");
 		
 		System.out.println("Pila: " + pilaStr);
 		System.out.println("Tope: " + pilaStr.top());
+		System.out.println("esta vacio?: " + pilaStr.isEmpty());
 		String itemTop = pilaStr.top();
 		pilaStr = pilaStr.pop();
 		System.out.println("Elemento sacado: " + itemTop);
 		System.out.println("Pila: " + pilaStr);
 		System.out.println("Tope: " + pilaStr.top());
+		System.out.println("esta vacio?: " + pilaStr.isEmpty());
+
+		System.out.println("Vaciando pila . . .");
+		pilaStr.emptyStack();
+		System.out.println("Pila: " + pilaStr);
+		System.out.println("Tope: " + pilaStr.top());
+		System.out.println("esta vacio?: " + pilaStr.isEmpty());
 	}
 }
 
